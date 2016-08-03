1e48db0a3e226124f45e7b577fbed8d1e96accf2
rknd-djamal <djamalrknd@gmail.com>
2016-08-03 14:07:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
+  $('.iconmenu').click(function() {
     $('.menu').animate({
       left: "0"
     }, 250);
@@ -11,15 +11,7 @@ var main = function() {
   });
 
   /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-200px"
-    }, 350);
 
-    $('body').animate({
-      left: "0px"
-    }, 350);
-  });
   $('.row').click(function() {
     $('.menu').animate({
       left: "-200px"
